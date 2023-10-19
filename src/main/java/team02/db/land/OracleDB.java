de40ff4d21c74295983ff6403244846ff03c0a94
seohwoo <tjguddn9899@naver.com>
2023-10-19 13:27:42 +0900

    10월 19일 초기 세팅

---
+++ src/main/java/team02/db/land/OracleDB.java
@@ -0,0 +1,50 @@
+package team02.db.land;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class OracleDB {
+
+	public Connection getConnection() {
+		Connection conn = null;
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			String url = "jdbc:oracle:thin:@192.168.219.123:1521/orcl";
+			String user = "team02";
+			String pw = "team";
+			conn = DriverManager.getConnection(url, user, pw);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return conn;
+	}// getConnection()
+
+	public void close(ResultSet rs, PreparedStatement pstmt, Connection conn) {
+		try {
+			if (rs != null) {
+				rs.close();
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		try {
+			if (pstmt != null) {
+				pstmt.close();
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		try {
+			if (conn != null) {
+				conn.close();
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}// close()
+}
